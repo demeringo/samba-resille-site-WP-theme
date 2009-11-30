921093c23db14e701e8af6164d49c68f665d074f
olivier <olivier@whitecat.lan>
2009-11-30 23:57:52 +0100

    Prev and next links to the same category in single view

--- single.php
+++ single.php
@@ -11,14 +11,26 @@ get_header();
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="navigation">
-			<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
-			<div class="alignright"><?php next_post_link('%link &raquo;') ?></div>
+			<div class="alignleft"><?php 
+			previous_post_link('&laquo; %link','%title'	,true) 
+			//previous_post_link('format','link','in_same_cat','excluded_categories'); 
+			?>
+			</div>
+			<div class="alignright"><?php 
+			next_post_link('%link &raquo;','%title',true) 
+			//next_post_link('format','link','in_same_cat','excluded_categories');
+			?></div>
+			
+			<?php  ?>
+
 		</div>
 
 		<!-- Custom code to force post template by category -->
-		<?php if ( in_category('4') ) {include(TEMPLATEPATH . '/singleEvenement.php');}
-		    	else {include(TEMPLATEPATH . '/singleOriginal.php');
-		    	}?>
+		<?php if ( in_category('4') ) {
+				include(TEMPLATEPATH . '/singleEvenement.php');
+			}else {
+				include(TEMPLATEPATH . '/singleOriginal.php');
+		}?>
 
 		<?php comments_template(); ?>
 
